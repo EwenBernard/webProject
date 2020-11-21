55171c6e2af6bb887d8e48d6fa1a2a42177ab850
Starlex92 <Starlex92@gmail.com>
2020-11-21 22:52:55 +0100

    home

--- client/components/Home.vue
+++ client/components/Home.vue
@@ -1,1 +1,54 @@
-zz
+<template>
+    <div id="app">
+        <h1>Annonces</h1>
+        <article v-for= "item in annonce">
+            <h2>{{ item.nameActivite }} - {{ item.nameUser }}</h2>
+            <p> {{ item.message }}</p>
+        </article>
+        <div id="pop-up">
+            <button class="button" @click="showModal = true">
+                X
+            </button>
+            <transition name="fade" appear>
+                <div class="modal-overlay" v-if="showModal" @click="showModal = false"></div>
+            </transition>
+            <transition name="slide" appear>
+                <div class="modal" v-if="showModal">
+                    <b>Annonce</b>
+                    <input placeholder="Description"></input>
+                    <button class="button" @click="showModal = false">
+                        X
+                    </button>
+                </div>
+            </transition>
+        </div>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        name:"Annonce",
+        props: {
+            annonces: { type: Object },
+            status: { type: Object }
+        },
+        data () {
+            return {
+            annonce:{
+                nameActivite: '',
+                nameUser: '',
+                message: ''
+            }, 
+            showModal: true,
+            status: O
+            }
+        },
+        async mounted () {
+        },
+        methods: {
+            annonce() {
+                this.$emit('annonce', this.annonce)
+            }
+        }
+    }         
+</script>
