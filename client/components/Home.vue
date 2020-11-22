2339c0cc19880f332e08e303fb56244dfcbf9db5
EwenBernard <73660111+EwenBernard@users.noreply.github.com>
2020-11-22 02:04:05 +0100

    Home fix

--- client/components/Home.vue
+++ client/components/Home.vue
@@ -1,7 +1,27 @@
 <template>
     <div id="app">
         <h1>Annonces</h1>
-        
+        <article v-for= "item in hometext" :key="item.id">
+            <h2>{{ item.title }} - {{ item.UserId }}</h2>
+            <p> {{ item.text }}</p>
+        </article>
+        <div id="pop-up">
+            <button class="button" @click="showModal = true">
+                +
+            </button>
+            <transition name="fade" appear>
+                <div class="modal-overlay" v-if="showModal" @click="showModal = false"></div>
+            </transition>
+            <transition name="slide" appear>
+                <div class="modal" v-if="showModal">
+                    <b>Annonce</b>
+                    <input placeholder="Description">
+                    <button class="button" @click="showModal = false">
+                        X
+                    </button>
+                </div>
+            </transition>
+        </div>
     </div>
 </template>
 
@@ -9,7 +29,7 @@
     module.exports = {
         name:"Home",
         props: {
-            annonces: { type: Object },
+            hometext: { type: Array},
             status: { type: Object },
             user: { type: Object }
         },
@@ -19,8 +39,7 @@
                 title:"",
                 text:"",
             }, 
-            showModal: true,
-            status: O
+            showModal: false
             }
         },
         async mounted () {
@@ -34,7 +53,7 @@
                 this.$emit('removeAnnonce', this.annonce)
             },
 
-            getUserId(){
+            async getUserId(){
                 const res = await axios.post('/api/me', user)
                 this.user = res.data
             }
